134d7039ebe5b938398fa61b96fef6a727595f79
Yuriy-Ihor <hefestsbunker@gmail.com>
2021-12-23 16:03:31 +0200

    Fix

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,11 +4,12 @@ import { PublicKey } from "@solana/web3.js";
 const SPIRIT_TOKEN_ADDRESS = new PublicKey("9bPoFPCwGCVGDMC5gvzisPdjgKC6tRLRDhirJvcktgVh");
 
 export const getSpiritBalance = async (wallet: PublicKey) => {
-    const response = await CONNECTION.getParsedTokenAccountsByOwner(wallet, 
+    const response: any = await CONNECTION.getParsedTokenAccountsByOwner(wallet, 
         {mint: SPIRIT_TOKEN_ADDRESS}    
     );
 
     if(!response || response == undefined) return 0;
+    if(!response.value[0].account || response.value[0].account == undefined) return 0;
 
     return response.value[0].account.data.parsed.info.tokenAmount.uiAmount;
 }
