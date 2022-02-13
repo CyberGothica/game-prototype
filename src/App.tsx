21c7a9a70c1fcc4276608d5eaef5ca2eba06bca5
UKuskus <65870521+UKuskus@users.noreply.github.com>
2022-02-13 21:17:15 +0200

    Cleaned project

--- src/App.tsx
+++ src/App.tsx
@@ -10,19 +10,10 @@ import { getNftsForOwner } from "./lib/solana/nft-utils";
 import { getTokenBalance } from "./lib/solana/utils";
 import { SPIRIT_TOKEN_ADDRESS } from "./lib/solana/config";
 
-
-
 //New Harmony
 
 import Web3 from 'web3'
 
-//import './App.css';
-//import Color from './contracts/Color.json'
-import * as ColorImported from './contracts/Color.json'
-// 
-import detectEthereumProvider from '@metamask/detect-provider';
-
-const Color: any = ColorImported
 declare var window: any
 
 const unityContext = new UnityContext({
@@ -36,23 +27,11 @@ const unityContext = new UnityContext({
 });
 
 export const App = () => {
-  /*constructor(props) {
-    super(props)
-    this.state = {
-      account: '',
-      contract: null,
-      totalSupply: 0,
-      colors: []
-    }
-  }*/
   const [account, setAccount]=useState<any>('')
-  const [contract, setContract]=useState<any>(null)
-  const [totalSupply, setTotalSupply]=useState<any>(0)
-  const [colors, setColors]=useState<any>([])
 
   const [displayWallets, setdisplayWallets] = useState(false);
   let userWallet: any;
-  console.log(Color)
+  
   useEffect(function () {
     unityContext.on("ConnectWallet", async function () {
       setdisplayWallets(true);
@@ -109,21 +88,7 @@ export const App = () => {
     await loadBlockchainData()
   }
     
-  /*
-  const provider = async () => {
-
-    await detectEthereumProvider();
-
-    if (provider) {
-      // From now on, this should always be true:
-      // provider === window.ethereum
-      App(provider); // initialize your app
-    } 
-    else {
-      console.log('Please install MetaMask!');
-    }
-  }
-  */
+  
   const loadWeb3 = async () => {
   //you code here
     if (window.ethereum) {
@@ -144,53 +109,12 @@ export const App = () => {
       const web3 = window.web3
       // Load account
       const accounts = await web3.eth.getAccounts()
-    // this.setState({ account: accounts[0] })
+  
       setAccount(accounts[0])
-      console.log(accounts[0])
-      const networkId = await web3.eth.net.getId()
-      //console.log(Color)
-      //console.log(ColorImported)
-      //console.log(networkId)
-      //console.log(Color.networks)
-      const networkData:any = Color.networks[networkId]
      
-      if(networkData) {
-        const abi = Color.abi
-        const address = networkData.address
-        console.log(networkData.address)
-        const contract = new web3.eth.Contract(abi, address)
-      // this.setState({ contract })
-        setContract(contract)
-
-        const totalSupply = await contract.methods.totalSupply().call()
-      // this.setState({ totalSupply })
-        setTotalSupply(totalSupply)
-        // Load Colors
-        for (var i = 1; i <= totalSupply; i++) {
-          const color = await contract.methods.colors(i - 1).call()
-          //this.setState({
-        //   colors: [...this.state.colors, color]
-        // })
-          setColors([...colors,color])
-        }
-      } else {
-        window.alert('Smart contract not deployed to detected network.')
-      }
-    
+      console.log(accounts[0])
+      setdisplayWallets(false);
     }
-    
-     const mint = (color: any) => {
-    // this.state.contract.methods.mint(color).send({ from: this.state.account })
-    // .once('receipt', (receipt) => {
-    //   this.setState({
-    //     colors: [...this.state.colors, color]
-    //   })
-    // })
-    contract.methods.mint(color).send({from: account})
-      .once('receipt',(receipt: any)=>{
-        setColors([...colors,color])
-      })
-  }
     
  
 
@@ -220,20 +144,7 @@ export const App = () => {
     }
   }
 
-  // $('button').addClass("mintButton");
-
-  // let observerTargetDom = document.body;
-  // let observerConfig = { attributes: true, childList: true, subtree: true };
-  // const mutationHandler = (mutationList: any, observer: any) => {
-  //   $('button').addClass("walletButton");
-  //   console.log("hi");
-  // };
-  // let mutationObserver = new MutationObserver(mutationHandler);
-  // try {
-  //   mutationObserver.observe(observerTargetDom, observerConfig);
-  // } catch (e) {
-  //   console.log(e);
-  // }
+  
 
   return (
     <>
